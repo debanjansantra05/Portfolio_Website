d395b403a9ea70ab2f34b42133b198ee9d4d1234
debanjansantra05 <debanjansantra2014@gmail.com>
2026-04-21 13:31:38 +0530

    update

--- contact.js
+++ contact.js
@@ -8,3 +8,33 @@ const menu = document.getElementById("nav-menu");
 toggle.addEventListener("click", () => {
     menu.classList.toggle("active");
 });
+
+// FOOTER YEAR
+document.getElementById("year").textContent = new Date().getFullYear();
+
+function updateClock() {
+    const now = new Date();
+
+    let hours = now.getHours().toString().padStart(2, '0');
+    let minutes = now.getMinutes().toString().padStart(2, '0');
+    let seconds = now.getSeconds().toString().padStart(2, '0');
+
+    document.getElementById("time-text").innerText = `${hours}:${minutes}:${seconds}`;
+
+    // DATE
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    };
+
+    document.getElementById("date").innerText =
+        now.toLocaleDateString('en-IN', options);
+
+    // YEAR
+    document.getElementById("year").innerText = now.getFullYear();
+}
+
+setInterval(updateClock, 1000);
+updateClock();
--- script.js
+++ script.js
@@ -82,5 +82,32 @@ if (marquee) {
     });
 }
 
-// FOOTER 
+// FOOTER YEAR
 document.getElementById("year").textContent = new Date().getFullYear();
+
+function updateClock() {
+    const now = new Date();
+
+    let hours = now.getHours().toString().padStart(2, '0');
+    let minutes = now.getMinutes().toString().padStart(2, '0');
+    let seconds = now.getSeconds().toString().padStart(2, '0');
+
+    document.getElementById("time-text").innerText = `${hours}:${minutes}:${seconds}`;
+
+    // DATE
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    };
+
+    document.getElementById("date").innerText =
+        now.toLocaleDateString('en-IN', options);
+
+    // YEAR
+    document.getElementById("year").innerText = now.getFullYear();
+}
+
+setInterval(updateClock, 1000);
+updateClock();
